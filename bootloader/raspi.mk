51b066ab611a6da90a0ec2812d53d023602f4ed6
Brad Robinson <brad@toptensoftware.com>
2023-06-29 08:58:45 +1000

    Make file tweaks

--- bootloader/raspi.mk
+++ bootloader/raspi.mk
@@ -6,32 +6,32 @@ ifeq ($(strip $(PLATFORM)),rpi1)
 RASPI=1
 AARCH=32
 TARGETNAME=kernel.img
-#ARCHFLAGS=-mcpu=arm1176jzf-s -marm -mfpu=vfp -mfloat-abi=hard
+ARCHFLAGS?=-mcpu=arm1176jzf-s -marm -mfpu=vfp -mfloat-abi=hard
 else ifeq ($(strip $(PLATFORM)),rpi2)
 RASPI=2
 AARCH=32
 TARGETNAME=kernel7.img
-#ARCHFLAGS=-mcpu=cortex-a7 -marm -mfpu=neon-vfpv4 -mfloat-abi=hard
+ARCHFLAGS?=-mcpu=cortex-a7 -marm -mfpu=neon-vfpv4 -mfloat-abi=hard
 else ifeq ($(strip $(PLATFORM)),rpi3)
 RASPI=3
 AARCH=32
 TARGETNAME=kernel8-32.img
-#ARCHFLAGS=-mcpu=cortex-a53 -marm -mfpu=neon-fp-armv8 -mfloat-abi=hard
+ARCHFLAGS?=-mcpu=cortex-a53 -marm -mfpu=neon-fp-armv8 -mfloat-abi=hard
 else ifeq ($(strip $(PLATFORM)),rpi4)
 RASPI=4
 AARCH=32
 TARGETNAME=kernel7l.img
-#ARCHFLAGS=-mcpu=cortex-a72 -marm -mfpu=neon-fp-armv8 -mfloat-abi=hard
+ARCHFLAGS?=-mcpu=cortex-a72 -marm -mfpu=neon-fp-armv8 -mfloat-abi=hard
 else ifeq ($(strip $(PLATFORM)),rpi3-64)
 RASPI=3
 AARCH=64
 TARGETNAME=kernel8.img
-#ARCHFLAGS=-mcpu=cortex-a53 -mlittle-endian
+ARCHFLAGS?=-mcpu=cortex-a53 -mlittle-endian
 else ifeq ($(strip $(PLATFORM)),rpi4-64)
 RASPI=4
 AARCH=64
 TARGETNAME=kernel8-rpi4.img
-#ARCHFLAGS=-mcpu=cortex-a72 -mlittle-endian
+ARCHFLAGS?=-mcpu=cortex-a72 -mlittle-endian
 else
 $(error Unknown platform '$(PLATFORM)')
 endif
@@ -55,18 +55,18 @@ endif
 MAKEFLAGS += --no-print-directory
 
 # C Options
-GCC_COMMONFLAGS = -nostdlib -nostartfiles -ffreestanding $(ARCHFLAGS)
+GCC_COMMONFLAGS += -nostdlib -nostartfiles -ffreestanding $(ARCHFLAGS)
 DEFINE += AARCH=$(AARCH) AARCH$(AARCH) RASPI=$(RASPI) RASPI$(RASPI)
 
 # Project Kind
-PROJKIND = custom
+PROJKIND ?= custom
 HEXFILE = $(TARGET:%.img=%.hex)
 LIBGCC	  := "$(shell $(PREFIX)gcc $(ARCHFLAGS) -print-file-name=libgcc.a)"
 EXTRALIBS += $(LIBGCC)
 ifeq ($(AARCH),32)
-LINKSCRIPT = link_script
+LINKSCRIPT ?= link_script
 else
-LINKSCRIPT = link_script_64
+LINKSCRIPT ?= link_script_64
 endif
 
 # Main Rules
@@ -76,26 +76,26 @@ include ../Rules/Rules.mk
 # Link
 $(TARGET): $(PRECOMPILE_TARGETS) $(OBJS) $(LINKPROJECTLIBS)
 	@echo "  LD    $(notdir $@)"
-	@$(PREFIX)ld -o $@ $^ $(LIBS) $(GCC_LIBS) $(EXTRALIBS) -T $(LINKSCRIPT) --no-warn-rwx-segments -Map $(@:%.img=%.map) -o $(@:%.img=%.elf)
-	@$(PREFIX)objdump -D $(@:%.img=%.elf) > $(@:%.img=%.lst)
-	@$(PREFIX)objcopy $(@:%.img=%.elf) -O binary $@
+	$(Q)$(PREFIX)ld -o $@ $^ $(LIBS) $(GCC_LIBS) $(EXTRALIBS) -T $(LINKSCRIPT) --no-warn-rwx-segments -Map $(@:%.img=%.map) -o $(@:%.img=%.elf)
+	$(Q)$(PREFIX)objdump -D $(@:%.img=%.elf) > $(@:%.img=%.lst)
+	$(Q)$(PREFIX)objcopy $(@:%.img=%.elf) -O binary $@
 
 # Hex file
 $(HEXFILE): $(TARGET)
 	@echo "  COPY  $(notdir $(HEXFILE))"
-	@$(PREFIX)objcopy $(<:%.img=%.elf) -O ihex $@
+	$(Q)$(PREFIX)objcopy $(<:%.img=%.elf) -O ihex $@
 
 # 32-bit targets
 aarch32:
-	@$(MAKE) PLATFORM=rpi1
-	@$(MAKE) PLATFORM=rpi2
-	@$(MAKE) PLATFORM=rpi3
-	@$(MAKE) PLATFORM=rpi4
+	$(Q)$(MAKE) PLATFORM=rpi1
+	$(Q)$(MAKE) PLATFORM=rpi2
+	$(Q)$(MAKE) PLATFORM=rpi3
+	$(Q)$(MAKE) PLATFORM=rpi4
 
 # 64-bit targets
 aarch64:
-	@$(MAKE) PLATFORM=rpi3-64
-	@$(MAKE) PLATFORM=rpi4-64
+	$(Q)$(MAKE) PLATFORM=rpi3-64
+	$(Q)$(MAKE) PLATFORM=rpi4-64
 
 # 32 and 64-bit targets
 aarch: aarch32 aarch64
