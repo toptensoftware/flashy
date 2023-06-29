3a6dca538332deb78842bf2d74e0cf9b17745134
Brad Robinson <brad@toptensoftware.com>
2023-06-30 09:59:18 +1000

    Build script

--- build.sh
+++ build.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-# Clock version number
-cd flashy ; npm version patch
+## Clock version number
+#cd flashy ; npm version patch
+#
+## Build bootloader
+#cd bootloader ; make cleanall ; make bootloader.zip
+#
+## Package it
+#cd flashy ; npm publish --access public
+#
+## Commit
+#git add .
+#VERSION=`node -p require\(\"./flashy/package.json\"\).version`
+#git commit -m "Build $VERSION"
+#git push
 
-# Build bootloader
-cd bootloader ; make cleanall ; make bootloader.zip
 
-# Package it
-cd flashy ; npm publish --access public
+git commit --dry-run -a
+echo $?
