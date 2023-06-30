46e042baf561b64024e8c568b431f7ad1f7f5c3a
Brad Robinson <brad@toptensoftware.com>
2023-06-30 10:09:23 +1000

    Build script

--- build.sh
+++ build.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 if git commit --dry-run -a > /dev/null; then
-echo "Uncommitted changes"
-exit
+    echo "Uncommitted changes"
+    exit
 fi
     
 # Clock version number
-cd flashy ; npm version patch
+(cd flashy ; npm version patch)
 
 # Build bootloader
-cd bootloader ; make cleanall ; make bootloader.zip
+(cd bootloader ; make cleanall ; make bootloader.zip)
 
-# Package it
-cd flashy ; npm publish --access public
+## Package it
+(cd flashy ; npm publish --access public)
 
 # Commit
-git add .
 VERSION=`node -p require\(\"./flashy/package.json\"\).version`
+git add .
 git commit -m "Build $VERSION"
 git push
 
