8be316cfe8c0859a74227cfd56290854c61ea2ac
Brad Robinson <brad@toptensoftware.com>
2023-07-30 17:39:57 +1000

    tab completion in interactive shell

--- flashy/cmd_shell.js
+++ flashy/cmd_shell.js
@@ -1,6 +1,69 @@
 let nodeRepl = require('node:repl');
 let chalk = require('chalk');
 
+// Find the position in a command line string of the last argument
+function findLastArg(line)
+{
+    let i = 0;
+    let len = line.length;
+    let lastArgPos = 0;
+    let pendingQuote = null;
+    while (i < len)
+    {
+        switch (line[i])
+        {
+            case ' ':
+            case '\t':
+            case '\r':
+            case '\n':
+                i++;
+                lastArgPos = i;
+                break;
+
+            case '\\':
+                i+=2;
+                break;
+                
+            case '\"':
+                i++;
+                while (i < len && line[i] != '\"')
+                    i++;
+                if (i<len)
+                    i++;
+                else
+                    pendingQuote = '\"';
+                break;
+        
+            case '\'':
+                i++;
+                while (i < len && line[i] != '\'')
+                    i++;
+                if (i<len)
+                    i++;
+                else
+                    pendingQuote = '\'';
+                break;
+
+            case '&':
+            case '|':
+            case ';':
+            case '(':
+            case ')':
+                i++;
+                lastArgPos = i;
+                return;
+
+            default:
+                i++;
+                break;
+        }
+    }
+    return { 
+        lastArgPos,
+        pendingQuote
+    }
+}
+
 async function run(ctx)
 {
     // Wait for device
@@ -62,11 +125,51 @@ async function run(ctx)
         // Notify finished
         callback(null);
     }
+
+    async function completer(line, callback)
+    {
+        try
+        {
+
+            // Get the last argument
+            let lastArgInfo = findLastArg(line);
+            let lastArg = line.substring(lastArgInfo.lastArgPos);
+    
+            // If any special characters then can't match anything
+            if (lastArg.match(/[\*\?\(\)\&\|\<\>]/))
+                return [[], line];
+    
+            // Work out the glob pattern by closing any pending
+            // opening quote and appending the wildcard character
+            let glob = lastArg;
+            if (lastArgInfo.pendingQuote)
+                glob += lastArgInfo.pendingQuote;
+            glob += "*";
+    
+            // Get matching files
+            let matches = await ctx.layer.exec_ls(rwd, "ls -ald " + glob, true);
+    
+            function escape_match(name)
+            {
+                if (lastArgInfo.pendingQuote)
+                    return `${lastArgInfo.pendingQuote}${name}${lastArgInfo.pendingQuote}`;
+                
+                return name.replace(/[\ \t\r\n\*\?\(\)\&\|\<\>]/g, "\\$&");
+            }
+    
+            callback(null, [matches.map(x => escape_match(x.name)), lastArg]);
+        }
+        catch
+        {
+            callback(null, [[], ""]);
+        }
+    }
       
     // Start the repl
     repl = nodeRepl.start({ 
         prompt: format_prompt(), 
-        eval: invoke_command 
+        eval: invoke_command,
+        completer: completer,
     }); 
 
     // Handle Ctrl+C/Ctrl+D, '.exit',
